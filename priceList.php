21ed1fb606fc27c0a56f851307156d00562d27b6
Omar Rosales <omargerardoguerrero@gmail.com>
2019-08-15 11:53:50 -0500

    Cambiar lista de precios 1,2,3 con AJAX

--- priceList.php
+++ priceList.php
@@ -1,28 +1,34 @@
 <?php include 'head.php'; ?>
 
+
 <div class="sectionTitle">LISTA DE PRECIOS</div>
 <div class="sButtons"><a href="listaPrecios.php" class="formButton blueB" target="_blank"><i class='fa fa-file-pdf-o' aria-hidden='true'></i> PDF</a></div>
-<div class="searchDiv">Buscar <input type="search" id="searchInput" class="inputText" style="width:300px"></div>
+<div class="searchDiv">Buscar <input type="search" id="searchInput" class="inputText" style="width:300px">
+Selecciona la lista<select id="selectW" class="" name="">
+		<option value='0'>Lista 1</option>
+	    <option value='1'>Lista 2</option>
+        <option value='2'>Lista 3</option>
+</select><br>
+</div>
 <table id="priceList" width="100%" border="0" cellspacing="0" cellpadding="0" class="dataTable">
   <thead>
     <tr>
       <td width="200px">C&oacute;digo</td>
       <td>Nombre</td>
-      <td>Categor&iacute;a</td>
       <td>Proveedor</td>
       <td>Precio</td>
     </tr>
   </thead>
   <tbody>
     <?php
-		$myQuery = $db->query("SELECT T1.ID, T1.code, T1.name product, T3.name vendor, T1.price, T2.catName cat FROM PRODUCT T1 JOIN CAT T2 ON T1.catID = T2.ID JOIN VENDOR T3 ON T1.vendorID = T3.ID WHERE T1.active = 'Y' ORDER BY T1.name ASC");
+      /*LISTA 1 DE PRECIOS*/
+		$myQuery = $db->query("SELECT T1.ID code1,T1.REFERENCE code,T1.NAME product,T1.PRICESELL price FROM products T1");
 		while($row = $myQuery->fetch()){			
 			echo "
 				<tr>
 				  <td>".$row["code"]."</td>
 				  <td>".$row["product"]."</td>
-				  <td>".$row["cat"]."</td>
-				  <td>".$row["vendor"]."</td>
+				  <td>".$row["code1"]."</td>
 				  <td>$".$row["price"]."</td>
 				</tr>
 			";
@@ -35,6 +41,13 @@
 $('#priceList').filterTable({
 	inputSelector: '#searchInput'
 });
+    
+var sendValues = function() {
+    //llamamos a includes/priceLists.php para qeu haga la busqueda de valores de las otras listas,segun vayan seleccionando
+		table.ajax.url("includes/priceLists.php?wh="+$("#selectW").val()).load();
+	};
+    
+$("#selectW").on("change", sendvalues);
 </script>
     
 <?php include 'footer.php'; ?>
